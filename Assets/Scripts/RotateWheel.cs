af6015c61a9f3f27fd148dd4c526ad39fa214046
Sonika Baniya <akinos.ericon@gmail.com>
2018-11-18 12:43:48 +0545

    Wheel Rotated

---
+++ Assets/Scripts/RotateWheel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWheel : MonoBehaviour {
+
+public float speed=5f;
+
+
+	void Update (){
+		
+		transform.Rotate(0,0,speed);
+		
+	}	
+}
